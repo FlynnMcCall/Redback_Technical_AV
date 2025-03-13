67a6e1be38a30aa53d99846d02d522850e32f869
G56zL <flynn.mccall@icloud.com>
2025-03-13 23:10:23 +1100

    Modified CMakeLists.txt to compile Q4

--- Question-4/Question-4.cc
+++ Question-4/Question-4.cc
@@ -1,23 +1,9 @@
 #include <memory>
 #include <vector>
 
-#include "gmock/gmock.h"
-#include "gtest/gtest.h"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
 
-/// Question 4: This is an extension task. Look at the interface class Observer. This class's responsibility 
-/// is to observe and react any changes happening to the EventEmitter class, which emits an event via calling `emitEvent`. 
-/// 
-/// Your Task: You are to write a mock implementation using Google Tests of Observer to test that EventEmitter actually calls all its
-/// attached Observers. This is called Mock testing, which essentially tests the external APIs of classes, and 
-/// their relationship, rather than being concerned with the correctness of the overall application. You are to modify the CMakeLists.txt
-/// file at the root of this repository, to make Question-4 utilise Google Tests. You also need to modify this file, to implement the mock, and
-/// fix the test to utilise the mock and pass the test. 
-/// 
-/// More information about integrating Google Tests can be found here https://google.github.io/googletest/quickstart-cmake.html
-/// You can learn about Mock Testing here: https://google.github.io/googletest/gmock_for_dummies.html
-
-
-// Forward declaration. Don't worry about this.
 class Actor;
 
 
@@ -69,3 +55,17 @@ TEST(ActorTest, ObserversAreCalled) {
 		actor.EmitEvent();
 	}
 }
+
+
+
+/// Question 4: This is an extension task. Look at the interface class Observer. This class's responsibility 
+/// is to observe and react any changes happening to the EventEmitter class, which emits an event via calling `emitEvent`. 
+/// 
+/// Your Task: You are to write a mock implementation using Google Tests of Observer to test that EventEmitter actually calls all its
+/// attached Observers. This is called Mock testing, which essentially tests the external APIs of classes, and 
+/// their relationship, rather than being concerned with the correctness of the overall application. You are to modify the CMakeLists.txt
+/// file at the root of this repository, to make Question-4 utilise Google Tests. You also need to modify this file, to implement the mock, and
+/// fix the test to utilise the mock and pass the test. 
+/// 
+/// More information about integrating Google Tests can be found here https://google.github.io/googletest/quickstart-cmake.html
+/// You can learn about Mock Testing here: https://google.github.io/googletest/gmock_for_dummies.html
